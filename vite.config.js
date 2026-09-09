258055e36bdc181d30736dc8f33c13083380bc77
BECARIO SISTEMAS <becariosistemas@menshenmex.com>
2026-09-09 11:29:45 -0600

    Prueba porque no se visualiza otra vez

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Proyecto_Menshen/",
   server: {
     host: "0.0.0.0",
     port: 5173,
   },
 });
-
 
